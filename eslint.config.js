c016d86daaf662c82816222fb4f1cbf9c1dde284
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-03-13 21:08:04 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -26,3 +26,20 @@ export default tseslint.config(
     },
   },
 )
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
